6af0a97fc0511cddcb525f3434c4160251bdb2d5
ForLoop <meetnp1706@gmail.com>
2023-07-04 18:58:50 +0530

    again?

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(props) {
+  return (
+    <div className="header">
+      <button>back</button>
+      <button>cart</button>
+    </div>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+
+export default function Main() {
+  return (
+    <div>
+      <Header />
+      <h1>heo</h1>
+    </div>
+  );
+}
